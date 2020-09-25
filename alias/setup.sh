b443eea651c035977f55eb252c1d05556c874f83
Thierry Lacour <thi@praqma.net>
2020-09-25 13:45:04 +0200

    Change archive publising to a GH Release
    Fix indentation issue in publish.yml
    
    Fix tag in release asset name
    
    Added the git cherry pick task
    
    [Cherry-pick] Fixed some wordings
    
    Add restore kata
    
    Add restore from history to restore kata
    
    Fix restore kata title
    
    Add kata on aliases
    
    Add --global to unset command
    
    Clarify description of aliases
    
    Add part on reduce the risk for typos
    Add that alias can also be on a system level
    
    Add fake remote to advanced-rebase-interactive
    
    Added a step to 3-way-merge (#295)
    
    adding a new step for basic revert exercise (#296)
    
    Add feature to test all kata setup.sh scripts
    
    ./utils/test/test_setup.sh can be used to test a single kata
    Run it from the kata folder as:
    ../utils/test/test_setup.sh
    
    To test all katas, run ./test-all.sh from the root folder.
    
    This commit also includes a few bug-fixes that were caught by the test.
    In reverted-merge there is a merge operation in the setup that causes a
    conflict on purpose, and fixes it.
    I "wrapped" that in an `|| true` to make it not fail the test script.
    
    Run all tests in lexicographical order
    
    Better readability, when test output is listed in same order
    as in file system and on GitHub :-)
    
    Make the test.sh script actually fail!
    
    The test.sh has been rewritten to use the new test_setup.sh
    and thus actually causes a non-zero exit on failure.
    
    Use a function
    
    ShellCheck linting improvements
    
    Count & report no. of failed tests
    
    Run test-all.sh in pipeline
    
    For now, this only changes what is run in the bash tests on ubuntu & mac
    
    We need to addres the testing in ZSH seperately,
    as this is done in Docker
    
    Control default branch & avoid warning about this
    
    Since Git 2.28, caling git init will print out a long'ish warning
    strongly recommending to set init.defaultBranch in your global config.
    While this warning might be good in "real life" it is unnecessarily
    hostile to beginners, and also very verbose.
    
    The same version of Git introduced:
    
     --initial-branch=<branch-name>
    
     on git init, but using this would not be backwards compatible for
    people using git < 2.28.
    So I ame up with the solution of setting the new init.defaultBranch config
    inline whenever calling git init.
    
    In (nearly) all the .sh scripts, I refer to a $DEFAULT_BRANCH variable
    set in utils.sh that is sourced in (nearly) all setup scripts.
    
    For the PowerShell scripts, there isn't really a shared utils script
    the same way, so those are currently hardcoded to master, but easy to
    change for someone with more PS knowledge.
    
    Set user name & email in pipeline
    
    Maybe setting user.name & email globally will fix
    
    Add comment on cleaning up test artifacts
    
    Test on both bash and zsh
    
    - Allow control of which shell is used in test-all script
    - Add job to CI Action that tests on Zsh on mac
    - Cleanup GH actions naming & remove redundant Docker based zsh job
    
    Co-author: @zanderhavgaard
    
    Add GH action job with matrix of git versions
    
    first_commit

--- alias/setup.sh
+++ alias/setup.sh
@@ -9,8 +9,7 @@ make-exercise-repo
 echo "dummy" > dummy.txt
 git add dummy.txt
 git commit -m "dummy commit"
-git tag "dummy"
 echo "dummy2" > dummy.txt
 git add dummy.txt
 git commit -m "adding more content to dummy.txt"
-git tag -a "dummy2" -m "don't worry, be happy"
+
